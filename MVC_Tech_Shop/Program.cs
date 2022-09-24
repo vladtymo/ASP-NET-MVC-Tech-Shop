8d8b76eaaa657f9b62c6373ed622aee71366927d
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2022-09-24 12:37:10 +0300

    Move connection string to appsettings.json file.

--- MVC_Tech_Shop/Program.cs
+++ MVC_Tech_Shop/Program.cs
@@ -3,7 +3,7 @@ using MVC_Tech_Shop.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string connectionStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TechShopDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+string connectionStr = builder.Configuration.GetConnectionString("LocalDb");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
